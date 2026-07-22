60fce8a1323ddd45695fb8edd8f4e0ad1aa8c5e5
intellild <intellild@outlook.com>
2026-07-22 11:56:48 +0800

    intercept browser save shortcut

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import useEvent from "react-use/esm/useEvent";
 import { Toaster } from "sonner";
 import Editor from "@/components/Editor";
 import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 const App = () => {
+  useEvent(
+    "keydown",
+    (event: KeyboardEvent) => {
+      if ((event.ctrlKey || event.metaKey) && event.key.toLowerCase() === "s") {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    },
+    undefined,
+    { capture: true },
+  );
+
   return (
     <ThemeProvider defaultTheme="system">
       <div className="relative h-screen flex flex-col">
